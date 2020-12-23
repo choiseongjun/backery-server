c0c28315f1eed3a5927ab396e562a371c48f1a2f
seongjun Choi <csj2702@qlcinc.co.kr>
2020-12-23 14:30:40 +0900

    라이브러리 초기세팅

---
+++ src/main/java/com/bakery/pj/BackeryserverApplication.java
@@ -0,0 +1,29 @@
+package com.bakery.pj;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+@SpringBootApplication
+public class BackeryserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackeryserverApplication.class, args);
+	}
+//	@Bean
+//	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+//		SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+//		sessionFactory.setDataSource(dataSource);
+//		
+//		Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*Mapper.xml");
+//		sessionFactory.setMapperLocations(res);
+//		
+//		return sessionFactory.getObject();
+//	}
+}
